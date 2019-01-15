704eee7e254480ec105cdc0e387342f78b4c91f0
roula.m <roula.misrabi@kibarsoft.com>
2019-01-15 13:16:54 +0100

    fixed navigationBarTitleFont stored preperty in ALKConfiguration

--- Sources/Models/ALKConfiguration.swift
+++ Sources/Models/ALKConfiguration.swift
@@ -106,5 +106,13 @@ public extension ALKConfiguration {
     /// Navigation bar's title font. It will be used in all the
     /// ViewControllers where navigation bar is visible.
     /// If nil, the font attribute WON'T be added to navigation title text attributes
-    public var navigationBarTitleFont: UIFont? = nil
+    public var navigationBarTitleFont: UIFont? = nil {
+        get {
+            return self.navigationBarTitleFont
+        }
+        
+        set(newValue) {
+            self.navigationBarTitleFont = newValue
+        }
+    }
 }
